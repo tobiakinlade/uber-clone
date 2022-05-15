fc39c3b15324e53afa15981ef2ceb4fbbcb215e2
tobiakinlade <tobi.akinlade@yahoo.com>
2022-05-15 23:48:47 +0100

    initial commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import { View, Text, SafeAreaView, Image } from 'react-native';
+import tw from 'twrnc';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{ width: 100, height: 100, resizeMode: 'contain' }}
+          source={{
+            uri: 'https://links.papareact.com/gzs',
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
