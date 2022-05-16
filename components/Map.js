632915e615f48229a3800a937224314ff4c27362
tobiakinlade <tobi.akinlade@yahoo.com>
2022-05-16 19:28:49 +0100

    Map Marker

--- components/Map.js
+++ components/Map.js
@@ -3,12 +3,27 @@ import React from 'react';
 import MapView, { Marker } from 'react-native-maps';
 import { useSelector } from 'react-redux';
 import { selectOrigin } from '../slices/navSlice';
+import { selectDestination } from '../slices/navSlice';
 import tw from 'twrnc';
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import MapViewDirections from 'react-native-maps-directions';
+import { useRef } from 'react';
+import { useEffect } from 'react';
 const Map = () => {
   const origin = useSelector(selectOrigin);
+  const destination = useSelector(selectDestination);
+  const mapRef = useRef(null);
 
+  useEffect(() => {
+    if (!origin || !destination) return;
+
+    mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+    });
+  }, [origin, destination]);
   return (
     <MapView
+      ref={mapRef}
       style={tw`flex-1`}
       mapType='mutedStandard'
       initialRegion={{
@@ -18,6 +33,15 @@ const Map = () => {
         longitudeDelta: 0.0421,
       }}
     >
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          destination={destination.description}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor='black'
+        />
+      )}
       {origin?.location && (
         <Marker
           coordinate={{
@@ -27,6 +51,18 @@ const Map = () => {
           title='Origin'
           description={origin.description}
           identifier='origin'
+        />
+      )}
+
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          title='Origin'
+          description={destination.description}
+          identifier='destination'
         />
       )}
     </MapView>
